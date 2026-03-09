c95eed1076d2d8a6ff788720dcdecd446d850775
ahmetbulur34166 <166490199+ahmetbulur34166@users.noreply.github.com>
2026-03-09 12:40:03 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T09:34:27.790Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-09T09:40:03.518Z
+Commit: Fix broken links in documentation
