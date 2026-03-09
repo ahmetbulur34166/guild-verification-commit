66761478b1f1e4d2c33ed41fa1637f4a0d822999
ahmetbulur34166 <166490199+ahmetbulur34166@users.noreply.github.com>
2026-03-09 14:07:11 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T10:31:49.586Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-09T11:07:12.055Z
+Commit: Patch API endpoint error handling
