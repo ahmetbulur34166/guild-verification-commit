c90ccad570fae4a5cbd1c1dc8596c671eb9024eb
ahmetbulur34166 <166490199+ahmetbulur34166@users.noreply.github.com>
2026-03-09 14:21:12 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:21:08.135Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-09T11:21:13.467Z
+Commit: Resolve null pointer exception in user service
