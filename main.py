c7d6fceaa26037928748c8b5733f12aabd4ed172
ahmetbulur34166 <166490199+ahmetbulur34166@users.noreply.github.com>
2026-03-09 14:21:03 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:21:01.604Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-09T11:21:04.116Z
+Commit: Patch API endpoint error handling
